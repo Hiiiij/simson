d5630e206b548a6a7bd69b11757fcd652bea3dff
hiijs <bernadetta.nycz@icloud.com>
2019-04-17 18:55:20 +0200

    Updates

---
+++ precache-manifest.bc13e9ef09f37c86af6b840e74d6cfd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simson/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cad7b32b12a8b48e6b5b",
+    "url": "/simson/static/js/runtime~main.d8c0b105.js"
+  },
+  {
+    "revision": "822757e94f1e0dab0b03",
+    "url": "/simson/static/js/main.0e4d893e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/simson/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "822757e94f1e0dab0b03",
+    "url": "/simson/static/css/main.a0d166c3.chunk.css"
+  },
+  {
+    "revision": "44477144d607a3e495e926bf8178b8eb",
+    "url": "/simson/index.html"
+  }
+];
